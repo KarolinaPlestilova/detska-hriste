185096f47b690ffbf40d802c96ab434a63e1740d
KarolinaPlestilova <k.plestilova@gmail.com>
2020-06-20 14:06:58 +0200

    Deleted old pages and adds new SVG map

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router'
-import Page1 from './components/Page1';
-import Page2 from './components/Page2';
+import Map from './components/Map';
 import App from './App';
 import './index.html';
 
@@ -10,8 +9,7 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/page1', component: Page1 },
-    { path: '/page2', component: Page2 },
+    { path: '', component: Map },
   ],
 });
 
