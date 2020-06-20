ce58bdac2978b2f1ff643744a0b425fd1c6e92fa
KarolinaPlestilova <k.plestilova@gmail.com>
2020-06-20 19:50:14 +0200

    Old map deleting and created data list

---
+++ src/list.js
@@ -0,0 +1,83 @@
+export const countiesList = [
+  {
+    jihocesky: {
+      name: "Jihočeský kraj",
+      description: "Můj popis",
+      places: [
+        {
+          address: "Na Pakšovce, Písek, 397 01",
+          description: "Lidl hřiště"
+        },
+        {
+          address: "Topolová ulice, Prachatice, 383 01",
+          description: "Lidl hřiště"
+        }
+      ]
+    }
+  },
+  {
+    jihomoravsky: {
+      name: "Jihomoravský kraj"
+    }
+  },
+  {
+    karlovarsky: {
+      name: "Karlovarský kraj"
+    }
+  },
+  {
+    kralovehradecky: {
+      name: "Královehradecký kraj"
+    }
+  },
+  {
+    liberecky: {
+      name: "Liberecký kraj"
+    }
+  },
+  {
+    moravskoslezsky: {
+      name: "Moravskoslezský kraj"
+    }
+  },
+  {
+    olomoucky: {
+      name: "Olomoucký kraj"
+    }
+  },
+  {
+    pardubicky: {
+      name: "Pardubický kraj"
+    }
+  },
+  {
+    zlinsky: {
+      name: "Zlínský kraj"
+    }
+  },
+  {
+    plzensky: {
+      name: "Plzeňský kraj"
+    }
+  },
+  {
+    praha: {
+      name: "Praha"
+    }
+  },
+  {
+    stredocesky: {
+      name: "Středočeský kraj"
+    }
+  },
+  {
+    ustecky: {
+      name: "Ústecký kraj"
+    }
+  },
+  {
+    vysocina: {
+      name: "Vysočina"
+    }
+  }
+]
