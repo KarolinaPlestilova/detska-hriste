c0f896b043509da809901835bf27f7833a72c663
KarolinaPlestilova <k.plestilova@gmail.com>
2020-06-22 13:19:10 +0200

    CSS in Region detail

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <h1>Detail aktivity</h1>
-    <p>{{address}}</p>
-    <p>{{description}}</p>
+    <div>
+      <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+        <b-row no-gutters>
+          <b-col md="6">
+            <b-card-img src="https://picsum.photos/400/400/?image=20" alt="Image" class="rounded-0"></b-card-img>
+          </b-col>
+          <b-col md="6">
+            <b-card-body title="Detail aktivity">
+              <b-card-text>
+                <p>{{address}}</p>
+                <p>{{description}}</p>
+              </b-card-text>
+            </b-card-body>
+          </b-col>
+        </b-row>
+      </b-card>
+    </div>
   </div>
 </template>
 
--- src/components/Region.vue
+++ src/components/Region.vue
@@ -1,9 +1,43 @@
 <template>
+  <!-- <h1>Region detail - zde jsou vypsané všechny aktivity-hřiště z databáze dle názvu regionu (kraje)</h1> -->
   <div>
-    <!-- <h1>Region detail</h1> -->
-    <h2>{{ countyName }}</h2>
-    <p>{{ countyDescription }}</p>
-    <ul>
+    <div>
+      <h2>{{ countyName }}</h2>
+      <p>{{ countyDescription }}</p>
+    </div>
+
+    <b-card bg-variant="light" text-variant="blue" title="Card Title">
+      <b-card-text>With supporting text below as a natural lead-in to additional content.</b-card-text>
+      <b-button href="#" variant="primary">Go somewhere</b-button>
+    </b-card>
+
+    <router-link
+      v-for="(place, index) in places"
+      :to="{name: 'detail', params: {id: id, detailId: place.id, description: place.description, address: place.address}}"
+      v-bind:key="index"
+    >
+      <b-card bg-variant="light" text-variant="blue" :title="place.activity">
+        <b-card-text>{{ place.address }}</b-card-text>
+        <b-button variant="primary">KLIKNI</b-button>
+      </b-card>
+    </router-link>
+
+    <!-- Můj router link, který je ale proklikávací celý -->
+
+    <!-- <router-link
+      v-for="(place, index) in places"
+      :to="{name: 'detail', params: {id: id, detailId: place.id, description: place.description, address: place.address}}"
+      v-bind:key="index"
+    >
+      <b-card bg-variant="light" text-variant="blue" :title="place.activity">
+        <b-card-text>{{ place.address }}</b-card-text>
+        <b-button variant="primary">KLIKNI</b-button>
+      </b-card>
+    </router-link>-->
+
+    <!-- Původní router link z hackathon -->
+
+    <!-- <ul>
       <router-link
         v-for="(place, index) in places"
         tag="li"
@@ -12,7 +46,7 @@
       >
         <a>{{ place.address }}</a>
       </router-link>
-    </ul>
+    </ul>-->
   </div>
 </template>
 
