9c52836899cf37d3b2005058c5c8e5dae71337ca
KarolinaPlestilova <k.plestilova@gmail.com>
2020-06-22 09:29:53 +0200

    Adds bootstrap CSS and rename title App

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import Projekt from './components/Projekt';
 import App from './App';
 import './index.html';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
